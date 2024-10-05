8a13756245b3742631d052c02405126a43949329
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2024-10-05 18:17:15 +0200

    feat: preserve the flake in /etx

--- nixosModules/profiles/workstation.nix
+++ nixosModules/profiles/workstation.nix
@@ -40,5 +40,7 @@ in {
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     services.displayManager.defaultSession = "plasmax11";
+
+    hardware.enableAllFirmware = true;
   };
 }
