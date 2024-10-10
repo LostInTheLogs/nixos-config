fce488f4eee652c486283a16c21fc0cdcaeb7443
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2024-10-10 11:56:24 +0200

    feat: xdg vars

--- nixosModules/common/system.nix
+++ nixosModules/common/system.nix
@@ -24,6 +24,19 @@
     timeout = 2;
   };
 
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME = "$HOME/.local/bin";
+    PATH = [
+      "${XDG_BIN_HOME}"
+    ];
+  };
+
   services.tailscale.enable = true;
   services.openssh.enable = true;
 }
