83b49ade1ea446b7f5737e7d5d64104c621e35e2
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2024-10-16 16:26:42 +0200

    feat: locale

---
+++ nixosModules/common/locale.nix
@@ -0,0 +1,10 @@
+{...}: {
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "pl_PL.UTF-8/UTF-8"
+  ];
+  i18n.extraLocaleSettings = {
+    LC_TIME = "en_GB.UTF-8";
+  };
+}
