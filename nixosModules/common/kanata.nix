119f63389e012bdf2b1a698391799c28a265444f
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2024-10-17 19:40:20 +0200

    fix: kanata

--- nixosModules/common/kanata.nix
+++ nixosModules/common/kanata.nix
@@ -40,10 +40,10 @@ in {
           )
 
           (deflayer base
-            grv   1    2    3    4    5    6    7    8    9    0    -    =    bspc
-            esc   q    w    f    p    g    j    l    u    y    ;    [    ]    \
-            tab   a    r    s    t    d    h    n    e    i    o    '    ret
-            lsft  @z   x    c    v    b    k    m    ,    .    /    rsft
+            grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+            tab  @q   @w   e    r    t    y    u    i    @o   @p   [    ]    \
+            caps a    s    d    f    g    h    j    k    l    ;    '    ret
+            lsft @z   @x   c    v    b    n    m    ,    @.   @/   rsft
                                     spc
           )
         '';
