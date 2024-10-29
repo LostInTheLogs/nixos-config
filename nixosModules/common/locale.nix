0a375f7c053456507c5a31db24a6ccc28dce3551
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2024-10-29 12:02:36 +0100

    feat: hunspell

--- nixosModules/common/locale.nix
+++ nixosModules/common/locale.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
     "en_GB.UTF-8/UTF-8"
@@ -7,4 +7,6 @@
   i18n.extraLocaleSettings = {
     LC_TIME = "en_GB.UTF-8";
   };
+
+  environment.systemPackages = with pkgs; [hunspell hunspellDicts.pl_PL hunspellDicts.en_US hunspellDicts.en_GB-ise hunspellDicts.en_GB-ize];
 }
