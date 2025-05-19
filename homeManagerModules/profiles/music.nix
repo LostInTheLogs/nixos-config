96ab6df0fda29168b55be119a535a264a6c5c105
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2025-05-19 21:14:12 +0200

    feat: run-carla

--- homeManagerModules/profiles/music.nix
+++ homeManagerModules/profiles/music.nix
@@ -3,10 +3,22 @@
   lib,
   osConfig,
   ...
-}: {
+}: let
+  run-carla = pkgs.writeShellApplication {
+    name = "run-carla";
+    runtimeInputs = [
+      pkgs.my.carla
+    ];
+    text = ''
+      sudo ulimit -n 1000000
+      PIPEWIRE_LATENCY="2048/48000" carla "$HOME/Documents/music/all.carxp"
+    '';
+  };
+in {
   config = lib.mkIf osConfig.my.profiles.gaming.enable {
     home.packages = with pkgs; [
       my.carla
+      run-carla
       lsp-plugins
       calf
 
