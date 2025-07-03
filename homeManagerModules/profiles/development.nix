db4c9ca2228ec410a461567aeae6bf5eefe6f49c
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2025-07-03 19:57:40 +0200

    feat: atuin, hi emicake

--- homeManagerModules/profiles/development.nix
+++ homeManagerModules/profiles/development.nix
@@ -26,5 +26,15 @@
           pip
         ]))
     ];
+
+    programs.atuin = {
+      enable = true;
+      settings = {
+        auto_sync = true;
+        sync_frequency = "5m";
+        sync_address = "https://api.atuin.sh";
+        search_mode = "fuzzy";
+      };
+    };
   };
 }
