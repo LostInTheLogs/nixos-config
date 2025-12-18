630c91194e14d187fde71881339276fd40ba27c1
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2025-12-18 13:55:32 +0100

    feat: wake on lan

--- hosts/home/home.nix
+++ hosts/home/home.nix
@@ -85,6 +85,18 @@
   };
 
   # boot.kernelPackages = pkgs.linuxPackages_lqx;
+  boot.kernelPatches = [
+    {
+      name = "wake on lan";
+      patch = ./alx-wol_v6.12.patch;
+      # https://github.com/AndiWeiss/alx-wol/blob/master/patches/alx-wol_v6.12.patch
+      # :%s@v6\.12[^/]*/alx/@drivers/net/ethernet/atheros/alx/@g
+      # :g/^diff -u/d
+      # :%s@^--- @--- a/@
+      # :%s@^+++ @+++ a/@
+    }
+  ];
+
   musnix.soundcardPciId = "00:1f.3";
 
   system.stateVersion = "24.11";
