06e49e17b9bc491aa6ee3a339c584f07a568a9a9
LostNeophyte <110467150+LostNeophyte@users.noreply.github.com>
2026-01-01 13:00:03 +0100

    no cd=z

--- homeManagerModules/programs/terminal/zsh.nix
+++ homeManagerModules/programs/terminal/zsh.nix
@@ -24,7 +24,6 @@
       cloc = "tokei";
       ls = "EZA_ICON_SPACING=2 eza -a --icons --group-directories-first";
       ll = "EZA_ICON_SPACING=2 eza -al --icons --group-directories-first";
-      cd = "z";
     };
     oh-my-zsh = {
       enable = true;
