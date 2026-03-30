8bd402d9635d657a2b355eb3a3388701252c4cf0
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2026-03-30 16:13:00 +0200

    jetbrains

--- homeManagerModules/profiles/development.nix
+++ homeManagerModules/profiles/development.nix
@@ -10,7 +10,7 @@
       just
       gnumake
 
-      jetbrains.idea
+      my.jetbrains.idea
       jetbrains.rider
       icu
       (with dotnetCorePackages;
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   carla = pkgs.libsForQt5.callPackage ./carla {};
   iosevka-custom = pkgs.callPackage ./iosevka-custom {};
+  jetbrains = (
+    lib.recurseIntoAttrs (
+      pkgs.callPackages ./applications/editors/jetbrains {
+        vmopts = config.jetbrains.vmopts or null;
+        jdk = pkgs.jetbrains.jdk;
+      }
+    )
+  );
 }
