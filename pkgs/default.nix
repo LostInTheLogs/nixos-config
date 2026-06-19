5ce0fb3d802e78f06c24512ca48a3cdf11ff72da
LostInTheLogs <110467150+LostInTheLogs@users.noreply.github.com>
2026-06-19 14:36:51 +0200

    twemoji

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,6 +6,7 @@
 }: {
   carla = pkgs.libsForQt5.callPackage ./carla {};
   iosevka-custom = pkgs.callPackage ./iosevka-custom {};
+  twemoji = pkgs.callPackage ./twemoji {};
   jetbrains = (
     lib.recurseIntoAttrs (
       pkgs.callPackages ./applications/editors/jetbrains {
